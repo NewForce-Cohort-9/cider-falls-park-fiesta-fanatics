ebc4b7c61cb17ff2c7ff3053a5a355dbfdd7f09d
rustonray <rustonray@gmail.com>
2024-05-03 15:21:01 -0400

    getting info from main branch

---
+++ cider-falls/scripts/locations.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { getLocations } from "./database.js"
+const locations = getLocations()
+
+
+export const Locations = () => {
+    let html = "<ul>"
+
+    for (const location of locations) {
+        html += `<li>${location.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}*/
