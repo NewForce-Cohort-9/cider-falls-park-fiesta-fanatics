57c58a355e662b3bf82d1cb81183b80e7a7a1eb1
rustonray <rustonray@gmail.com>
2024-05-03 11:05:00 -0400

    finished adding services in database

--- cider-falls/scripts/database.js
+++ cider-falls/scripts/database.js
@@ -15,9 +15,35 @@ const database ={
     ],
 
     services: [ {
-
-    }
+        id: 1,
+        name: "rafting",
+    }, {
+        id: 2,
+        name: "canoeing",
+    }, {
+        id: 3,
+        name: "fishing",
+    }, {
+        id: 4,
+        name: "hiking",
+    }, {
+        id: 5,
+        name: "picnicking",
+    }, {
+        id: 6, 
+        name: "rock climbing",
+    }, {
+        id: 7, 
+        name: "lodging",
+    }, {
+        id: 8,
+        name: "parking",
+    }, {
+        id: 9,
+        name: "information"
+    }, {
+        id: 10,
+        name: "zip lines"
+    },
     ],
-
-
 }
