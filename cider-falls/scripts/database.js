14addbddd4c4e2304db687ae152132e6d79aa930
Jacob Dorris <jcbdrrs@gmail.com>
2024-05-03 13:36:19 -0500

    guests and attractions in database

--- cider-falls/scripts/database.js
+++ cider-falls/scripts/database.js
@@ -95,10 +95,25 @@ const database ={
         id: 6,
         location: "southeast",
     }],
-
-
+    
     attractions: [ {
+        id: 1,
+        name: "Chamfort River",
+        
+        id: 2,
+        name: "Lost Wolf Hiking Trail",
 
+        id: 3,
+        name: "Lodge",
+
+        id: 4,
+        name: "Gander River",
+
+        id: 5,
+        name: "Campgrounds",
+        
+        id: 6,
+        name: "Pine Bluffs Trails",
     }
     ],
 
