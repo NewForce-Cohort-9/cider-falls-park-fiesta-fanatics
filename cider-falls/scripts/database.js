662ca7ba1ed96bc6435cedc85fe029c43a768855
Katherine Schloe <KatherineSchloe@outlook.com>
2024-05-03 13:59:29 -0400

    added export and import for locations

--- cider-falls/scripts/database.js
+++ cider-falls/scripts/database.js
@@ -139,3 +139,7 @@ const database ={
 export const getGuests = () => {
     return database.guests.map(guest => ({...guest}))
 }
+
+export const getLocations = () => {
+    return database.locations.map(location => ({...location}))
+}
