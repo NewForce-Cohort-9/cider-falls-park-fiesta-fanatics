6a616dbe31c56969a91797e18dc4b560657419df
Jacob Dorris <jcbdrrs@gmail.com>
2024-05-03 13:56:28 -0500

    added curly brackets on attractions

--- cider-falls/scripts/database.js
+++ cider-falls/scripts/database.js
@@ -96,26 +96,26 @@ const database ={
         location: "southeast",
     }],
     
-    attractions: [ {
+    attractions: [{
         id: 1,
         name: "Chamfort River",
-        
+    }, {
         id: 2,
         name: "Lost Wolf Hiking Trail",
-
+    }, {
         id: 3,
         name: "Lodge",
-
+    }, {
         id: 4,
         name: "Gander River",
-
+    }, {
         id: 5,
         name: "Campgrounds",
-        
+    }, {  
         id: 6,
         name: "Pine Bluffs Trails",
-    }
-    ],
+    }, {
+    }],
 
     services: [ {
         id: 1,
