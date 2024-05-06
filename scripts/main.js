0dc0318fe3c22feb2df13175a68bb337e840e14a
Jacob Dorris <jcbdrrs@gmail.com>
2024-05-06 09:55:49 -0500

    created main.js file and imported main.js to index.html

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+import { Attractions } from "./attractionLocations.js"
+import { Locations } from "./locations.js"
+import { Guests } from "./guests.js"
+import { Services } from "./serviceLocations.js"
+
+const mainContainer = document.querySelector("#container")
+
+const applicationHTML = `
+<h1>Shipping Ship Ships</h1>
+<article class="details">
+    <section class="detail--column list details__attractions">
+        <h2>Attractions</h2>
+        ${Attractions()}
+    </section>
+    <section class="detail--column list details__locations">
+        <h2>Locations</h2>
+        ${Locations()}
+    </section>
+    <section class="detail--column list details__guests">
+        <h2>Guests</h2>
+        ${Guests()}
+    </section>
+    <section class="detail--column list details__services">
+        <h2>Services</h2>
+        ${Services()}
+    </section>
+</article>
+`
+
+mainContainer.innerHTML = applicationHTML
+
